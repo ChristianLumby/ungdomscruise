0f16b902608ecf1e0e5c34318bf44ef68774885a
ChristianLumby <christian.k.lumby@gmail.com>
2018-11-25 19:05:29 +0100

    rutekort tilføjet

--- scripts/facebook_init.js
+++ scripts/facebook_init.js
@@ -1,5 +1,4 @@
 
-<script>
   window.fbAsyncInit = function() {
     FB.init({
       appId            : '210274199886393',
@@ -13,7 +12,6 @@
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     js.src = "https://connect.facebook.net/da_DK/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-</script>
